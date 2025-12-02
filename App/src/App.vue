996b1c1f98382a37ea5b5f7e0b3f62d8c77a2a8d
Gleb Syomichev <103398854+dirttop@users.noreply.github.com>
2025-12-02 15:11:47 -0500

    Forced Rebuild

--- App/src/App.vue
+++ App/src/App.vue
@@ -43,7 +43,7 @@ const handleSave = async (gameData: Partial<Game>) => {
   }
 };
 
-//add to force rebuild
+//add to force rebuild 2
 
 const handleEditGame = (game: Game) => {
   editingGame.value = game;
